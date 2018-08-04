058cd60d2f796458a836f64d10bfe8f68edcf3ad
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-08-04 21:00:58 +0700

    Reverse String: Solution 2

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,35 +7,43 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  // const arr = str.split('');
-  // arr.reverse();
-  // return arr.join('');
-  return str
-    .split('')
-    .reverse()
-    .join('');
+  let res = '';
+  for (let char of str) {
+    res = char + res;
+  }
 
-  // Solution 1
-  // const reversedArr = [];
-  // for (let i = str.length - 1; i >= 0; i--) {
-  //   reversedArr.push(str[i]);
-  // }
-  // return reversedArr.join('');
-  // Solution 2
-  // let res = '';
-  // for (let i = str.length - 1; i >= 0; i--) {
-  //   res += str[i];
-  // }
-  // return res;
-  // Solution 3
-  // const strArr = [...str];
-  // let tmp;
-  // for (let i = 0; i < str.length / 2; i++) {
-  //   tmp = strArr[str.length - 1 - i];
-  //   strArr[str.length - 1 - i] = strArr[i];
-  //   strArr[i] = tmp;
-  // }
-  // return strArr.join('');
+  return res;
 }
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   const arr = str.split('');
+//   arr.reverse();
+//   return arr.join('');
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+//   Solution 1
+//   const reversedArr = [];
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversedArr.push(str[i]);
+//   }
+//   return reversedArr.join('');
+//   Solution 2
+//   let res = '';
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     res += str[i];
+//   }
+//   return res;
+//   Solution 3
+//   const strArr = [...str];
+//   let tmp;
+//   for (let i = 0; i < str.length / 2; i++) {
+//     tmp = strArr[str.length - 1 - i];
+//     strArr[str.length - 1 - i] = strArr[i];
+//     strArr[i] = tmp;
+//   }
+//   return strArr.join('');
+// }
