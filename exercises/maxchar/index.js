273316b1f6274ecbe4cdfa1872efd63904eb73aa
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-08-06 23:24:59 +0700

    Max Char: My Solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -8,3 +8,15 @@
 function maxChar(str) {}
 
 module.exports = maxChar;
+
+// function maxChar(str) {
+//   const chars = {};
+//   for (let char of str) {
+//     chars[char] = chars[char] + 1 || 1;
+//   }
+
+//   const values = Object.keys(chars).map(char => chars[char]);
+//   const max = Math.max(...values);
+
+//   return Object.keys(chars).filter(char => chars[char] === max)[0];
+// }
