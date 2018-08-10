fb4ae23a10bd906a519133cd91ffd38005c3682d
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-08-10 22:32:56 +0700

    Capitalized String: My Solution - Solution 1

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -10,3 +10,12 @@
 function capitalize(str) {}
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//   const words = [];
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+
+//   return words.join(' ');
+// }
