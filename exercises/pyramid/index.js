db3ef6406adb2472d9826385e4cc2f7f06200757
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-08-11 17:30:20 +0700

    Pyramid: Solution 2

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,7 +14,27 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+  const midpoint = n - 1;
+
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+
+  pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
 
