5f4012130355a009924ec93354d4e894322f30b7
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-08-15 10:10:31 +0700

    Fibonacci: Solution 2 - Recursive optimize

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,13 +8,32 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function memoize(fn) {
+  console.log(fn);
+  const cache = {};
+  return function(...args) {
+    console.log(args);
+    if (cache[args]) {
+      console.log(cache[args]);
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
   if (n < 2) {
     return n;
   }
 
   return fib(n - 1) + fib(n - 2);
 }
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
 
