ea2ab7079de9a84841d892730356d806c2294199
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-08-18 09:41:14 +0700

    LinkedList size

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -18,6 +18,18 @@ class LinkedList {
     const node = new Node(data, this.head);
     this.head = node;
   }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+
+    while (node) {
+      count++;
+      node = node.next;
+    }
+
+    return count;
+  }
 }
 
 module.exports = { Node, LinkedList };
