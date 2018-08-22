6c6077569b9768cccbb912abac0c35d28adf6718
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-08-22 21:41:32 +0700

    Level Width: Solution

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,23 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const array = [root, 's']; // s is stopper to identify level of tree
+  const counter = [0];
+
+  while (array.length > 1) {
+    const node = array.shift();
+
+    if (node === 's') { // when node equal s, it means we are completed search at current level
+      array.push('s');
+      counter.push(0);
+    } else {
+      array.push(...node.children);
+      counter[counter.length - 1]++;
+    }
+  }
+
+  return counter;
+}
 
 module.exports = levelWidth;
