a9182537a5d2e8203ff70b3e4eea55abe7dbcae7
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-08-25 12:48:43 +0700

    Selection Sort: Implementation

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -16,7 +16,23 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
 
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      const larger = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = larger;
+    }
+  }
+
+  return arr;
 }
 
 function mergeSort(arr) {
