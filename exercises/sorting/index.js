8f82d7b571875f1241c3572b298134240dbaff5b
Lanh Hoang <lanh.hoangcong@gmail.com>
2018-08-25 17:21:54 +0700

    Merge Sort: Implementation

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -36,10 +36,18 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
 
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
-function merge(left, right) {
+function merge(left, right) { // left and right are 2 sorted array
   const results = [];
 
   while (left.length && right.length) {
